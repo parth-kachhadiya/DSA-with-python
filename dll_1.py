6baa3089abaf07b12c4cf5345bc919493b260b55
parth kachhadiya <parthkachhadiya04@gmail.com>
2024-11-10 10:03:04 +0530

    Latest modification

--- dll_1.py
+++ dll_1.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    print("Currently this python file is reserved for Dubble linked list")
+class Node:
+    def __init__(self,data,previous = None,next = None):
+        self.data = data
+        self.previous = previous
+        self.next = next
+
+class DLL:
+    def __init__(self,head = None):
+        self.head = head
+
+    def
