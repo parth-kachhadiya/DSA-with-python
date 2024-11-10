3fa60104643884d9047e86cd8ce4742cd5c23bc2
parth kachhadiya <parthkachhadiya04@gmail.com>
2024-11-10 16:49:52 +0530

    Dublee linked lis

--- dll_1.py
+++ dll_1.py
@@ -15,6 +15,113 @@ class DLL:
             self.head.previous = node
         self.head = node
 
+    def insert_at_end(self,data):
+        node = Node(data)
+        if self.is_empty():
+            self.head = node
+        else:
+            iterator = self.head
+            while iterator.next:
+                iterator = iterator.next
+            
+            iterator.next = node
+            node.previous = iterator
+
+    def insert_after_given_value(self,value,new_value):
+        if not self.is_empty():
+            iterator = self.head
+            while iterator.data != value and iterator.next:
+                iterator = iterator.next
+
+            if iterator.data != value:
+                print("No data found")
+            else:
+                node = Node(new_value)
+                node.next = iterator
+                node.previous = iterator.previous
+                iterator.previous.next = node
+                iterator.previous = node
+
+        else:
+            print("Linked list is already empty..!")
+
+    """ > - <> - <> - <> - <> - <> - <> - < Update > - <> - <> - <> - <> - <> - <    """
+    def update_at_begin(self,new_value):
+        if not self.is_empty():
+            self.head.data = new_value
+        else:
+            print("Linked list is already empty..!")
+
+    def update_at_end(self,new_value):
+        if not self.is_empty():
+            iterator = self.head
+            while iterator.next:
+                iterator = iterator.next
+            iterator.data = new_value
+        else:
+            print("Linked list is already empty..!")
+
+    def update_at_given_value(self,value,new_value):
+        if not self.is_empty():
+            iterator = self.head
+            while iterator.data != value and iterator.next:
+                iterator = iterator.next
+
+            if iterator.data != value:
+                print("No data found")
+            else:
+                iterator.data = new_value
+        else:
+            print("Linked list is already empty..!")
+
+    """ > - <> - <> - <> - <> - <> - <> - < Delete > - <> - <> - <> - <> - <> - < """
+    def delete_from_first(self):
+        if not self.is_empty():
+            if self.head.next == None:
+                self.head = None
+            else:
+                self.head = self.head.next
+                self.head.previous = None
+        else:
+            print("Linked list is already empty..!")
+
+    def delete_from_end(self):
+        if not self.is_empty():
+            iterator = self.head
+            if not iterator.next:
+                self.head = None
+            else:
+                while iterator.next:
+                    iterator = iterator.next
+                iterator.previous.next = None
+                iterator.previous = None
+        else:
+            print("Linked list is already empty..!")
+            
+    def delete_this_node(self,data):
+        if not self.is_empty():
+            iterator = self.head
+            while iterator.data != data and iterator.next:
+                iterator = iterator.next
+            if iterator.data != data:
+                print("Data doesn't exist")
+            else:
+                if not iterator.next and not iterator.previous: # If there is only one node
+                    self.head = None
+                elif iterator.previous and not iterator.next: # If the node is at the last position
+                    iterator.previous.next = None
+                    iterator.previous = None
+                elif not iterator.previous and iterator.next: # If the node is at first position
+                    self.head = self.head.next
+                    self.head.previous = None
+                else: # Between, at any position
+                    iterator.previous.next = iterator.next
+                    iterator.next.previous = iterator.previous
+
+        else:
+            print("Linked list is already empty..!")
+
+    """ > - <> - <> - <> - <> - <> - <> - < Helper functions > - <> - <> - <> - <> - <> - < """
     def is_empty(self):
         return self.head == None
 
@@ -26,7 +133,7 @@ class DLL:
                 iterator = iterator.next
             print()
         else:
-            print("Linked list is already empty..!")
+            print("Linked list is already empty")
 
     def start_iteration_from_end(self):
         if not self.is_empty():
@@ -42,8 +149,10 @@ class DLL:
             print("Linked list is already empty..!")
 
 obj = DLL()
-obj.start_iteration_from_begin()
+obj.insert_at_begin(40)
 obj.insert_at_begin(30)
 obj.insert_at_begin(20)
 obj.insert_at_begin(10)
-obj.start_iteration_from_end()
+obj.start_iteration_from_begin()
+obj.delete_this_node(50)
+obj.start_iteration_from_begin()
